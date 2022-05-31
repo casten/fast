025216d0a0dfe7d01be66ff953f45ccab1b54452
Casten Riepling <casten.riepling@gmail.com>
2022-05-31 00:25:55 -0700

    Simplify
    * remove status
    * perform speed measurement in workers
    * use 1MB chunks
    * run 4 (25MB) measurements
    * don't do stabilization
    
    It's not perfect. but better than

--- fast.py
+++ fast.py
@@ -11,20 +11,16 @@ MAX_DURATION = 30
 STABILITY_DELTA = 2
 MIN_STABLE_MEASUREMENTS = 6
 
-total = 0
-done = 0
 sessions = []
 
 
 async def run():
-    print('fast.com cli')
     token = await get_token()
     urls = await get_urls(token)
     conns = await warmup(urls)
-    future = ensure_future(measure(conns))
-    result = await progress(future)
+    speed = await measure(conns)
     await cleanup()
-    return result
+    return speed
 
 
 async def get_token():
@@ -36,7 +32,6 @@ async def get_token():
         resp = await s.get(f'https://fast.com{script}')
         text = await resp.text()
         token = re.search(r'token:"(.*?)"', text).group(1)
-    dot()
     return token
 
 
@@ -45,7 +40,6 @@ async def get_urls(token):
         params = {'https': 'true', 'token': token, 'urlCount': 5}
         resp = await s.get('https://api.fast.com/netflix/speedtest', params=params)
         data = await resp.json()
-    dot()
     return [x['url'] for x in data]
 
 
@@ -58,60 +52,31 @@ async def get_connection(url):
     s = ClientSession()
     sessions.append(s)
     conn = await s.get(url)
-    dot()
     return conn
 
 
 async def measure(conns):
-    workers = [measure_speed(conn) for conn in conns]
-    await gather(*workers)
+    start = time()
+    res = []
+    for i in range(0,4):
+        workers = [measure_speed(conn) for conn in conns]
+        res.append(await gather(*workers))
 
+    elapsed = time() - start
+    total = sum(sum(res,[]))
+    speed =  (total*8/elapsed)/(1024*1024)
+    return speed
 
 async def measure_speed(conn):
-    global total, done
-    chunk_size = 64 * 2**10
+    chunk_size = 1024*10424
+    total = 0
     async for chunk in conn.content.iter_chunked(chunk_size):
         total += len(chunk)
-    done += 1
-
-
-def stabilized(deltas, elapsed):
-    return (
-        elapsed > MIN_DURATION and
-        len(deltas) > MIN_STABLE_MEASUREMENTS and
-        max(deltas) < STABILITY_DELTA
-    )
-
-
-async def progress(future):
-    start = time()
-    measurements = deque(maxlen=10)
-    deltas = deque(maxlen=10)
-
-    while True:
-        await sleep(0.2)
-        elapsed = time() - start
-        speed = total / elapsed / 2**17
-        measurements.append(speed)
-
-        print(f'\033[2K\r{speed:.3f} mbps', end='', flush=True)
-
-        if len(measurements) == 10:
-            delta = abs(speed - mean(measurements)) / speed * 100
-            deltas.append(delta)
-
-        if done or elapsed > MAX_DURATION or stabilized(deltas, elapsed):
-            future.cancel()
-            return speed
+    return total
 
 
 async def cleanup():
     await gather(*[s.close() for s in sessions])
-    print()
-
-
-def dot():
-    print('.', end='', flush=True)
 
 
 def main():
@@ -120,4 +85,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    print(f"{main()}Mbps")
